9ed3680af78a72c668fdb33341b61635308f5afc
Bleak Chandler <bleakchandler@gmail.com>
2021-01-19 21:16:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,10 +3,10 @@ def square_array(array)
   square_array = []
   counter = 0
   
- while square_array[counter] do
+ while array[counter] do
   array[counter] = (array[counter]**2)
   counter += 1
 
 end
-  
+  array
 end
